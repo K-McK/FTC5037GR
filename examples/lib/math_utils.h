4489b57b261bdc3da43a94c4bffb23bf1b2f1a48
Stephen <patrick.stephen@gmail.com>
2013-08-30 22:21:05 -0500

    adding example

---
+++ examples/lib/math_utils.h
@@ -0,0 +1,19 @@
+/**
+ *
+ *  Filename:  math_utils.h
+ *
+ *  Description:  a collection of math operations
+ *
+ *  Copyright 2013, Got Robot! FTC Team 5037
+ *
+ */
+#ifndef MATH_UTILS_H
+#define MATH_UTILS_H
+     
+/** macros */
+#define product(X, Y) ((X) * (Y))
+#define sum(X, Y) ((X) + (Y))
+#define min(X, Y) ((X) < (Y) ? (X) : (Y))
+#define max(X, Y) ((X) > (Y) ? (X) : (Y))
+
+#endif /* !MATH_UTILS_H */
---
+++ examples/math_example.c
@@ -0,0 +1,10 @@
+#inlcude "stdio.h";
+#include "lib/math_utils.h";
+
+void main()
+{
+
+  printf("Max is %d", max(1,2));
+  printf("Min is %d", min(1,2));
+
+}
