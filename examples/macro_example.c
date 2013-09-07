dfcc273021b6aa21775181413c3a0316fb6439be
Stephen <patrick.stephen@gmail.com>
2013-09-07 11:48:14 -0500

    adding comments

--- examples/macro_example.c
+++ examples/macro_example.c
@@ -1,8 +1,14 @@
-#include "stdio.h";
+#include <stdio.h>;
 #include "lib/math_utils.h";
 
 void main()
 {
+
+  /**
+   * the following block of code shows the use of
+   * the min() and max() macros that are defined
+   * in the header file lib/math_utils.h
+   */
   printf("Max is %d", max(1,2));
   printf("Min is %d", min(1,2));
 }
