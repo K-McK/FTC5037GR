473517021dcc38d9c946b950b26b1f1e3f47a5a0
Stephen <patrick.stephen@gmail.com>
2013-09-24 18:38:55 -0500

    model of how we will implement custom functions

---
+++ examples/auto_example.c
@@ -0,0 +1,18 @@
+/**
+ *
+ *  Filename:  auto_example.c (this should be the name of the function)
+ *
+ *  Description:  Enter a description of what this function does
+ *
+ *  Copyright 2013, Got Robot! FTC Team 5037
+ *
+ */
+
+#include "foo.h"
+
+task main()
+{
+  /* this is how we invoke the included function */
+  int test1 = foo(FOO_FOO);
+  int test2 = foo(DOO_DOO);
+}
---
+++ examples/lib/foo.h
@@ -0,0 +1,37 @@
+/**
+ *
+ *  Filename:  foo.h
+ *
+ *  Description:  determines if you want foofoo or doodoo
+ *
+ *  Copyright 2013, Got Robot! FTC Team 5037
+ *
+ */
+#ifndef FOO_H
+#define FOO_H
+     
+/** constants etc that foo() needs */
+#define FOO_FOO 1
+#define DOO_DOO 2
+
+
+/**
+ * this is an example function
+ */
+void foo(int value)
+{
+  if(value == FOO_FOO)
+  {
+    return 111;
+  }
+  else if(value == DOO_DOO)
+  {
+    return 222;
+  }
+  else
+  {
+    return 333;
+  }
+}
+
+#endif /* !FOO_H */
