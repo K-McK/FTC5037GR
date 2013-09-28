5b5e68fb34f1ef60c94e5acfca7229912ec25c0d
Patrick Stephen <pj@stephentakeover.com>
2013-09-28 15:34:48 -0500

    Changed file to work with swing turns

--- src/lib/abs_swing_turn_to.h
+++ src/lib/abs_swing_turn_to.h
@@ -1,14 +1,17 @@
 /**
 *
-*  Filename:  abs_swing_turn_to.h (this should be the name of the function)
+*  Filename:  abs_swing_turn_to.h
 *
-*  Description:  Enter a description of what this function does
+*  Description:  The hedder file that alows you to do a swing turn
 *
 *  Copyright 2013, Got Robot! FTC Team 5037
 *
 */
-#ifndef ABS_SWING_TURN TO_H
-#define ABS_SWING_TURN TO_H
+#ifndef ABS_SWING_TURN_TO_H
+#define ABS_SWING_TURN_TO_H
+
+#include "xzander/hitechnic-gyro.h"
+/** macros */
 
 typedef enum
 {
@@ -16,8 +19,52 @@ typedef enum
 	COUNTERCLOCKWISE
 } e_direction;
 
-//macros
-
-void abs_swing_turn_to(int dergrees,e_direction dir,int speed)
+//=======================================
+// Swing Turn
+//=======================================
+void abs_point_turn_to(int degree,e_direction dir,int speed)
 {
-	float rotspeed(
+	int i = 0;
+	float rotSpeed = 0;
+	float heading = 0;
+
+	HTGYROstartCal(HTGYRO);
+
+	time1[T1] = 0;
+
+	while (time1[T1] < 20)
+		wait1Msec(1);
+
+	time1[T1]=0;
+
+if(dir == COUNTERCLOCKWISE)
+	{
+		motor[right_motor] = speed;
+		motor[left_motor] = 0;
+	}
+	else
+	{
+		motor[right_motor] = 0;
+		motor[left_motor] = speed;
+	}
+
+	while(i < 5)
+	{
+		if (heading > degree) i++;
+
+		rotSpeed = HTGYROreadRot(HTGYRO);
+
+		heading += rotSpeed * 0.02;
+
+		nxtDisplayCenteredBigTextLine(3, "%2.0f", heading);
+	}
+	motor[right_motor] = 0;
+	motor[left_motor] = 0;
+}
+
+//#define product(X, Y) ((X) * (Y))
+//#define sum(X, Y) ((X) + (Y))
+//#define min(X, Y) ((X) < (Y) ? (X) : (Y))
+//#define max(X, Y) ((X) > (Y) ? (X) : (Y))
+
+#endif /* !MATH_UTILS_H */
