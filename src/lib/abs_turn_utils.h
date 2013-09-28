1a991b924f618b76aaa52ee91fd856fa9ac83029
Patrick Stephen <pj@stephentakeover.com>
2013-09-28 15:51:25 -0500

    added a turn utils

---
+++ src/lib/abs_turn_utils.h
@@ -0,0 +1,20 @@
+/**
+ *
+ *  Filename:  abs_turn_utils.h
+ *
+ *  Description:  a collection of math operations
+ *
+ *  Copyright 2013, Got Robot! FTC Team 5037
+ *
+ */
+#ifndef ABS_TURN_UTILS_H
+#define ABS_TURN_UTILS_H
+
+/** macros */
+typedef enum
+{
+	CLOCKWISE,
+	COUNTERCLOCKWISE
+} e_direction;
+
+#endif /* !ABS_TURN_UTILS */
