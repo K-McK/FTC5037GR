dfe2ee85f550be133bae682b996d19a2f0e41b03
Patrick Stephen <pj@stephentakeover.com>
2013-10-01 20:14:32 -0500

    made global file to hold all of the global variables and edited abs_create_mem.h to create emory

--- src/lib/abs_create_mem.h
+++ src/lib/abs_create_mem.h
@@ -1,20 +1,21 @@
 /**
  *
- *  Filename:  math_utils.h
+ *  Filename:  abs_create_mem.h
  *
  *  Description:  a collection of math operations
  *
  *  Copyright 2013, Got Robot! FTC Team 5037
  *
  */
-#ifndef MATH_UTILS_H
-#define MATH_UTILS_H
+#ifndef ABS_CREATE_MEM_H
+#define ABS_CREATE_MEM_H
 
 /** macros */
-#define gyro_degrees(X) (X)
-#define product(X, Y) ((X) * (Y))
-#define sum(X, Y) ((X) + (Y))
-#define min(X, Y) ((X) < (Y) ? (X) : (Y))
-#define max(X, Y) ((X) > (Y) ? (X) : (Y))
+int memory_block[48];
 
-#endif /* !MATH_UTILS_H */
+int abs_create_mem()
+{
+	return memory_block;
+}
+
+#endif /* !ABS_CREATE_MEM_H */
