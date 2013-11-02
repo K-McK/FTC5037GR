718b8d35dbbba72ee6146a9c8317923a031fda45
Patrick Stephen <pj@stephentakeover.com>
2013-11-02 14:40:50 -0500

    Fixed joystick stuff

---
+++ src/lib/abs_joystick_gunner.h
@@ -0,0 +1,26 @@
+/**
+*
+*  @file abs_joystick_drive.h
+*
+*  @brief The header file that handles the joystick motor control
+*
+*  @copyright Copyright 2013, Got Robot? FTC Team 5037
+*
+*/
+#ifndef ABS_JOYSTICK_GUNNER_H
+#define ABS_JOYSTICK_GUNNER_H
+
+/** macros */
+//=======================================
+// joystick drive
+//=======================================
+void abs_joystick_gunner()
+{
+	if(abs(joystick.joy2_y1) > 10) 	motor[block_grabber_left] = joystick.joy2_y1*100/127;
+	else motor[block_grabber_left] = 0;
+
+	if(abs(joystick.joy2_y2) > 10) motor[block_grabber_right] = joystick.joy2_y2*100/127;
+	else motor[block_grabber_right] = 0;
+}
+
+#endif /* !ABS_JOYSTICK_DRIVE_H */
