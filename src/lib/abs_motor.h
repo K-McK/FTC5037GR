62d1089f5a8c416b504292047048391c474a3033
Patrick Stephen <pj@stephentakeover.com>
2013-11-09 19:20:16 -0600

    added a lift for auto

---
+++ src/lib/abs_motor.h
@@ -0,0 +1,27 @@
+/**
+*
+*  @file template.h (this should be the name of the function)
+*
+*  @brief  Enter a description of what this function does
+*
+*  @param
+*
+*  @return
+*
+*  @copyright Copyright 2013, Got Robot! FTC Team 5037
+*/
+#ifndef ABS_MOTOR_H
+#define ABS_MOTOR_H
+
+void abs_move(e_motor_move move_type, int power)
+{
+	if(move_type == FLAG)
+	{
+		motor(flag_motor)==power
+	}
+	else
+	{
+		motor(lifter_motor)==power
+	}
+}
+#endif /* !ABS_MOTOR_H */
