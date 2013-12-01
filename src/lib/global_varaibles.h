e853d271c5d14cbade48594d7b8b13fa7e5d370a
Patrick Stephen <pj@stephentakeover.com>
2013-11-30 20:19:45 -0600

    more...

--- src/lib/global_varaibles.h
+++ src/lib/global_varaibles.h
@@ -158,13 +158,12 @@ const int target_angle = 110;
 ubyte accelermoeter_reads = 0;
 int accelermoeter_array [] = {0,1,2,3,4,5,6,7,8,9,10,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
 ubyte accelermoeter_total_value = 0;
-int accelermoeter_average = 0;
+int accelermoeter_average = 0;\
 
-//-----------------------------
-// misc
-//-----------------------------
-
-/*
+int sensor_num = 1;
+int sensor_max = 4;
+int sensor_value = 0;
+/**
 *
 *  @def st_gyro
 *     The reference value for the sensor in smoke test
@@ -175,9 +174,6 @@ int accelermoeter_average = 0;
 *  @def st_tilt
 *     The reference value for the sensor in smoke test
 */
-int sensor_num = 1;
-int sensor_max = 4;
-int sensor_value = 0;
 
 #define st_gyro 1
 #define st_IR 2
@@ -207,6 +203,8 @@ string sensor_list [] = {
 *  @def s_cal_time
 *     Tells the robot the screen state number for this screen statestate
 *  @def s_gyro_cal
+*     Tells the robot the screen state number for this screen statestate
+*  @def s_ready
 *     Tells the robot the screen state number for this screen statestate
 *  @def s_delay_wait
 *     Tells the robot the screen state number for this screen statestate
