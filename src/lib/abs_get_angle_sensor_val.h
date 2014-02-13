0cb6b93982ef2ec443b1530cf95541ca3dc7cd37
Patrick Stephen <pj@stephentakeover.com>
2014-02-12 21:54:47 -0600

    Changed flow charts and added get angle sensor

---
+++ src/lib/abs_get_angle_sensor_val.h
@@ -0,0 +1,23 @@
+/**
+ *
+ *  @file template.h (this should be the name of the function)
+ *
+ *  @brief  Enter a description of what this function does
+ *
+ *  @param
+ *
+ *  @return
+ *
+ *  @copyright Copyright 2013, Got Robot! FTC Team 5037
+ */
+#ifndef TEMPLATE_H
+#define TEMPLATE_H
+typedef enum
+{
+	RELITIVE,
+	TOTAL
+} e_val_type;
+
+int get_angle_sensor_val(e_val_type val_type)
+
+#endif /* !TEMPLATE_H */
