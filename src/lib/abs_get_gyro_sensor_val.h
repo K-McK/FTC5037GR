f66dbd2593a7fbb60e410b510b60cfdeafa703f1
pstephen43 <patrick.stephen@gmail.com>
2014-02-21 00:58:56 -0600

    adding compiler flag options

--- src/lib/abs_get_gyro_sensor_val.h
+++ src/lib/abs_get_gyro_sensor_val.h
@@ -19,10 +19,6 @@ int abs_get_gyro_sensor_val(e_gyro_val_type gyro_val)
 {
 	static int last_gyro_read_time = 0;
 	static int last_gyro_read_val = 0;
-	static bool queue_empty = true;
-	static int current_index = 0;
-
-	if(current_index == GYRO_VALUE_QUEUE_SIZE) { current_index = 0; }
 
 	int current_time = nPgmTime;
 	int last_val;
@@ -43,6 +39,12 @@ int abs_get_gyro_sensor_val(e_gyro_val_type gyro_val)
 	{
 		last_gyro_read_val = HTGYROreadRot(HTGYRO) - g_original_gyro_val;
 	}
+
+#if DEBUG_MODE == 1
+
+	static bool queue_empty = true;
+	static int current_index = 0;
+	if(current_index == GYRO_VALUE_QUEUE_SIZE) { current_index = 0; }
 
 	/**
 	* if the queue is empty, initialize all values to the first reading
@@ -71,6 +73,7 @@ int abs_get_gyro_sensor_val(e_gyro_val_type gyro_val)
 		}
 		abs_dlog(__FILE__, "Gyro Queue ", "middle_value_avg %d = ", middle_value_avg());
 	}
+#endif
 
 	if(g_gyro_ran == true)
 	{
@@ -82,11 +85,9 @@ int abs_get_gyro_sensor_val(e_gyro_val_type gyro_val)
 		}
 	}
 
-	if(g_gyro_ran == false)
-		g_gyro_ran = true;
+	if(g_gyro_ran == false) g_gyro_ran = true;
 
 	return last_gyro_read_val;
-	//return middle_value_avg();
 }
 
 #endif /* !ABS_GET_GYRO_SENSOR_VAL_H */
