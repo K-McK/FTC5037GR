78bdc280209d90125029976d3e8205e189b10191
K-McK <odyssey4444@sbcglobal.net>
2014-03-27 21:50:02 -0500

    removed 2nd multilog

--- src/lib/abs_sensors.h
+++ src/lib/abs_sensors.h
@@ -146,7 +146,7 @@ task abs_sensors()
 		raw_gyro2 = abs_get_gyro_sensor_val(RAW,GYRO2);
 		abs_set_heading(RELATIVE, GYRO2, g_rel_heading2 + ((raw_gyro2 - (g_gyro2_drift+(g_delta_drift2*(float)(g_curr_time-g_prev_time)))) * (float)(g_curr_time-g_prev_time)/1000) + drift_compensation);
 		abs_set_heading(CONSTANT, GYRO2, g_const_heading2 + ((raw_gyro2 - (g_gyro2_drift+(g_delta_drift2*(float)(g_curr_time-g_prev_time)))) * (float)(g_curr_time-g_prev_time)/1000) + drift_compensation);
-		abs_log_multivalue(LIST2, g_rel_heading2);
+		//abs_log_multivalue(LIST2, g_rel_heading2);
 
 		//used gyro
 		if(g_gyro_use==GYRO1)
